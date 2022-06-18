cf14c0e962982053e334b0c995dd89e15f0173f5
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-06-18 12:49:46 -0300

    jest witch styled component configured and created Home test

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { screen } from '@testing-library/react';
+import { renderThemeProvider } from '../../styles/render-theme';
+import Home from '.';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('[Component]: <Home />', () => {
+  it('should be render Home correctly', () => {
+    renderThemeProvider(<Home />);
+    const headingContainer = screen.getByRole('heading', {
+      name: /Hello World!/i,
+    }).parentElement; //parentElement busca o pai do container
+    expect(headingContainer).toHaveStyle({
+      background: 'red',
+    });
+  });
+  it('should be render snapshot Home', () => {
+    renderThemeProvider(<Home />);
+    const headingContainer = screen.getByRole('heading', {
+      name: /Hello World!/i,
+    }).parentElement; //parentElement busca o pai do container
+    expect(headingContainer).toMatchSnapshot();
+  });
 });
